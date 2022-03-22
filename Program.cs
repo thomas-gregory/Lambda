e0c1a7326e1af34303f5e2007b0a75780cf882f8
ThomasGregory42 <84841134+thomas-gregory@users.noreply.github.com>
2022-03-22 13:17:10 +0000

    Changed Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,6 @@ Variable x = scope.CreateVariable("x");
 scope.CreateFunction("sin", new UnaryFunction(Math.Sin, true));
 scope.CreateFunction("coth", new UnaryFunction(y => 1 / Math.Tanh(y), true));
 scope.CreateFunction("ln", new UnaryFunction(Math.Log, true));
-Expression result = Parser.Parse(File.ReadAllText("test.txt"), scope);
+Expression result = Parser.Eval.Parser.Parse("sin(x)", scope);
 x.SetValue(0.1);
 Console.WriteLine(result.Evaluate());
